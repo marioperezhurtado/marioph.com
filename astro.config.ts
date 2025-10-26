d2674efd22669c4b938ef5c9536a3c908c0f3b27
Mario Perez Hurtado <marioph10@gmail.com>
2025-10-26 15:32:34 +0100

    wip: video conferencing with mediasoup post

--- astro.config.ts
+++ astro.config.ts
@@ -1,20 +1,22 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
-import mdx from '@astrojs/mdx'
-import rehypePrettyCode, { type Options } from 'rehype-pretty-code'
+import { defineConfig } from "astro/config";
+import tailwind from "@tailwindcss/vite";
+import mdx from "@astrojs/mdx";
+import rehypePrettyCode, { type Options } from "rehype-pretty-code";
 
 const prettyCodeOptions = {
-  theme: 'one-dark-pro',
+  theme: "one-dark-pro",
 } satisfies Options;
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://marioph.com',
+  site: "https://marioph.com",
   integrations: [
     mdx({
       syntaxHighlight: false,
       rehypePlugins: [[rehypePrettyCode, prettyCodeOptions]],
     }),
-    tailwind(),
-  ]
-})
+  ],
+  vite: {
+    plugins: [tailwind()],
+  },
+});
